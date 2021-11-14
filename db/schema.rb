fb6c9becf8502fd3c934e9417e85453c3879f7b4
rurumocchi <mocchi0316@gmail.com>
2021-11-14 07:56:40 +0000

    taskleaf4-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_122324) do
+ActiveRecord::Schema.define(version: 2021_11_14_025250) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
