7c90a97560ee75e59b53eec58d1b18c4edc244d9
rurumocchi <mocchi0316@gmail.com>
2021-11-16 10:28:46 +0000

    taskleaf4-9

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  
+
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   def show
@@ -14,13 +16,13 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
-  
+
   def update
     task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
-  
+
   def destroy
     task = Task.find(params[:id])
     task.destroy
@@ -28,7 +30,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(task_params.merge(user_id: current_user.id))
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
     else
@@ -41,4 +43,5 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
 end
